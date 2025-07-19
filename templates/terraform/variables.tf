edb81ab54c7a4e0b5166114fb3684d991dace77a
Vamsi Krishna <vamsikrishna@Tensure-VAKR.local>
2025-07-19 16:46:36 +0530

    added for testing

---
+++ templates/terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "project_id" {
+    description = "The GCP Project ID"
+    type        = string
+}
+
+variable "region" {
+    description = "The region to deploy resources"
+    type        = string
+}
